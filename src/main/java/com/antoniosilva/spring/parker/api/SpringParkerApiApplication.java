81b8066e85b9495f47e71a5026ee0277881eea3d
Antonio <antoniopsilva083@gmail.com>
2023-10-19 00:58:52 -0300

    start project

---
+++ src/main/java/com/antoniosilva/spring/parker/api/SpringParkerApiApplication.java
@@ -0,0 +1,13 @@
+package com.antoniosilva.spring.parker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParkerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParkerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antoniosilva/spring/parker/api/SpringParkerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antoniosilva.spring.parker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParkerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
