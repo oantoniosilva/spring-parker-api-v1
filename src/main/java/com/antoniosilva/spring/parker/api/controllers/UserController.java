75834a6672b08781f5c8c9d0fdeeb6636b1686d7
Antonio <antoniopsilva083@gmail.com>
2023-10-19 01:58:48 -0300

    feat: create resource user add and create method save user service

--- src/main/java/com/antoniosilva/spring/parker/api/controllers/UserController.java
+++ src/main/java/com/antoniosilva/spring/parker/api/controllers/UserController.java
@@ -1,6 +1,12 @@
 package com.antoniosilva.spring.parker.api.controllers;
 
+import com.antoniosilva.spring.parker.api.entities.User;
 import com.antoniosilva.spring.parker.api.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,11 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        var userPost = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userPost);
     }
 }
--- src/main/java/com/antoniosilva/spring/parker/api/services/UserService.java
+++ src/main/java/com/antoniosilva/spring/parker/api/services/UserService.java
@@ -1,7 +1,9 @@
 package com.antoniosilva.spring.parker.api.services;
 
+import com.antoniosilva.spring.parker.api.entities.User;
 import com.antoniosilva.spring.parker.api.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -9,5 +11,10 @@ public class UserService {
 
     public UserService(UserRepository userRepository)  {
         this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
     }
 }
