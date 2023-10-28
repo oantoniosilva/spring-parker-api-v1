85a0ff9ea827300fbd4442ec17007b2488868ab9
Antonio <antoniopsilva083@gmail.com>
2023-10-28 02:04:21 -0300

    feat: create resources find user, update password and list all users

--- src/main/java/com/antoniosilva/spring/parker/api/controllers/UserController.java
+++ src/main/java/com/antoniosilva/spring/parker/api/controllers/UserController.java
@@ -3,12 +3,10 @@ package com.antoniosilva.spring.parker.api.controllers;
 import com.antoniosilva.spring.parker.api.entities.User;
 import com.antoniosilva.spring.parker.api.services.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/users")
@@ -23,5 +21,23 @@ public class UserController {
     public ResponseEntity<User> create(@RequestBody User user) {
         var userPost = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userPost);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> updatePassword(@PathVariable Long id, @RequestBody User user) {
+        var newPassword = userService.changePassword(id, user.getPassword());
+        return ResponseEntity.ok(newPassword);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.listAll();
+        return ResponseEntity.ok(users);
     }
 }
--- src/main/java/com/antoniosilva/spring/parker/api/services/UserService.java
+++ src/main/java/com/antoniosilva/spring/parker/api/services/UserService.java
@@ -5,6 +5,8 @@ import com.antoniosilva.spring.parker.api.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -16,5 +18,24 @@ public class UserService {
     @Transactional
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("User not found!")
+        );
+    }
+
+    @Transactional
+    public User changePassword(Long id, String password) {
+        var user = findById(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> listAll() {
+        return userRepository.findAll();
     }
 }
